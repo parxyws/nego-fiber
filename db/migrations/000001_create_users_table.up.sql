b24fb283101c8eb3b8d8fbadd4ed7a79df5e909a
MociW <mochixmoca@gmail.com>
2025-12-24 08:15:48 +0700

    structuring project

---
+++ db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) UNIQUE NOT NULL,
+
+    full_name VARCHAR(255) NOT NULL,
+    phone_num VARCHAR(20) NOT NULL,
+
+)
